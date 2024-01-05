a9b019ad0186190193d1a4aad49a5224e97b7b52
Sebastian <55423389+Sebastian-RP@users.noreply.github.com>
2024-01-04 22:25:20 -0500

    permitir recibir archivos y validar el tamaño y tipo de archivo

---
+++ PeliculasApi/Validaciones/TipoArchivoValidacion.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PeliculasApi.Validaciones
+{
+    public class TipoArchivoValidacion: ValidationAttribute
+    {
+        private readonly string[] tiposValidos;
+
+        public TipoArchivoValidacion(string[] tiposValidos)
+        {
+            this.tiposValidos = tiposValidos;
+        }
+
+        public TipoArchivoValidacion(GrupoTipoArchivo grupoTipoArchivo) 
+        {
+            if (grupoTipoArchivo == GrupoTipoArchivo.Imagen)
+            {
+                tiposValidos = new string[] { "image/png", "image/jpeg", "image/jpg", "image/gif" };
+            }
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return ValidationResult.Success;
+            }
+
+            IFormFile formFile = value as IFormFile;
+
+            if (!tiposValidos.Contains(formFile.ContentType))
+            {
+                return new ValidationResult($"el tipo de archivo debe ser uno de los siguientes: {string.Join(", ", tiposValidos)}");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
